ed534c5c950e647d3edc4a255d798ba7838dbff1
Nurdan Abdiev <abdievv33@gmail.com>
2023-02-05 18:23:18 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Users from "./components/users";
+// import "./src/index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Users />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
